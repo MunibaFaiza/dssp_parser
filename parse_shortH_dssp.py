1a85c4b505f6788fedc9bacee1c49a98301140ec
Muniba Faiza <muniba.faiza@gmail.com>
2022-04-22 09:30:44 +0600

    Update parse_shortH_dssp.py

--- parse_shortH_dssp.py
+++ parse_shortH_dssp.py
@@ -1,3 +1,7 @@
+# Author: Dr. Muniba Faiza
+# Copyright Muniba Faiza 2022
+
+
 #!/usr/bin/env python3
 
 import os
@@ -22,12 +26,12 @@ file_list = os.listdir(input_path)
 output_path = os.getcwd()+"/shortH_output"
 
 
-#collecting the total number of log files in the directory.
-
+#collecting the total number of dssp files in the directory.
 num_files = len(glob.glob1(input_path,"*.dssp"))
 print('There are',num_files, 'dssp files in the current directory\n\n')
 
 
+#looking for short helices
 short_matches = ["G 3", "G 4", "G X","G >", "G <", "G  3", "G  X","G  >", "G  <", "G  4"]
 
 
@@ -38,9 +42,6 @@ for file_name in file_list:
 
 	if fnmatch.fnmatch(file_name, '*.dssp'):
 		with open(os.path.join(input_path, file_name), "r") as src_file:
-
-
-
 
 			line_was_helix = 0
 			filecounter = 0
@@ -68,8 +69,10 @@ for file_name in file_list:
 							filecounter +=1
 							set_line_header = line_header
 
-						
+						#change to output directory
 						os.chdir(output_path)
+						
+						#create output file and append helix to it.
 						with open(file_name+"_G_"+str(filecounter)+".dssp", "a") as g:
 								print(set_line_header+"\n"+line+"\n",file=g)
 								g.close()
